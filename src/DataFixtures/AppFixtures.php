5f54212d75049b7412d24372823f9d2cb04fa7a1
Lucas Delobelle <lucas@ld-web.net>
2020-01-10 17:43:52 +0100

    Fixtures w/ Faker

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+  public function load(ObjectManager $manager)
+  {
+    $faker = Factory::create();
+
+    for ($i = 0; $i < 100; $i++) {
+      $product = new Product();
+
+      $product->setName($faker->sentence(3, true))
+        ->setDescription($faker->paragraph(5))
+        ->setPriceHT($faker->randomFloat(2, 5, 300))
+        ->setStock($faker->numberBetween(0, 250))
+        ->setPromo($faker->boolean(20))
+        ->setPicture('https://source.unsplash.com/random/200x200');
+
+      $manager->persist($product);
+    }
+
+    $manager->flush();
+  }
+}
